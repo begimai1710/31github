92636f0dca66e49d2459db919cae4c31a7e7b73b
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-11-28 23:13:37 +0600

    uploaded variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+# integer variable
+age = 23
+height = 160
+weight = 58
+# string variable
+name = "Begimai"
+# boolean variable
+is_queen = True
+is_short = False
+# printing the variables
+# print(age)
+print(height)
+print(weight)
+print(name)
+print(is_queen)
+print(is_short)
