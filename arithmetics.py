5e335e0857b8436754bc7b879f474d7fb88b146a
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-11-30 22:00:00 +0600

    more arithmetic operations

--- arithmetics.py
+++ arithmetics.py
@@ -31,3 +31,38 @@ base = 10
 exponent = 2
 result = base ** exponent
 print(result)
+
+num = 100
+num += 20
+print(num) 
+
+count = 8
+count -= -2
+print(count) 
+
+# Assignments
+
+factor = 5
+value = 2
+value *= factor
+print(value)  
+
+total = 100
+divisor = 5
+total /= divisor
+print(total)  
+
+total = 50
+divisor = 6
+total //= divisor
+print(total) 
+
+number = 50
+divisor = 6
+number %= divisor
+print(number) 
+
+base = 2
+exponent = 4
+base **= exponent
+print(base) 
