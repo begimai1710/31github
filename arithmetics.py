b0fb39f03bdcd7f98eed91c9bf4236a90205c9ec
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-11-30 11:09:10 +0600

    ran basic arithmetic operations

---
+++ arithmetics.py
@@ -0,0 +1,33 @@
+b = 200
+result = a + b
+print(result)
+
+x = 100
+y = 200
+result = x - y
+print(result)
+
+x = 100
+y = 200
+result = x * y
+print(result)
+
+p = 100
+q = 200
+result = p / q
+print(result)
+
+p = 200
+q = 3
+result = p // q
+print(result)
+
+num = 200
+divisor = 3
+remainder = num % divisor
+print(remainder)
+
+base = 10
+exponent = 2
+result = base ** exponent
+print(result)
