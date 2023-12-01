585fbfddbe13b636416878e40c9cec00d5034130
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-01 22:48:56 +0600

    lists- sorting

---
+++ lists.py
@@ -0,0 +1,11 @@
+numbers = [5, 2, 1, 3, 9]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)  
+
+numbers = [5, 2, 1, 3, 9]
+sorted_numbers_desc = sorted(numbers, reverse= True)
+print(sorted_numbers_desc)  
+
+numbers = [5, 2, 1, 1, 9]
+numbers.sort()
+print(numbers)  
