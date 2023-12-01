9f3f7ee4f4e6df6a4e9713b5766d14b3bb79726d
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-01 22:56:45 +0600

    lists- adding elements

--- lists.py
+++ lists.py
@@ -9,3 +9,12 @@ print(sorted_numbers_desc)
 numbers = [5, 2, 1, 1, 9]
 numbers.sort()
 print(numbers)  
+
+# Adding elements to the list
+letters = ["a", "b", "c"]
+letters.append("f")
+print(letters)  
+
+fruits = ["apple", "banana", "orange"]
+fruits.insert(2, "cherry")  
+print(fruits) 
