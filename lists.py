63cf56a90f0b0be72329aceb6131f2eaf01d3e09
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-01 23:10:23 +0600

    lists- indexing

--- lists.py
+++ lists.py
@@ -18,3 +18,13 @@ print(letters)
 fruits = ["apple", "banana", "orange"]
 fruits.insert(2, "cherry")  
 print(fruits) 
+
+colors = ["red", "green", "blue"]
+print(colors[0])  
+
+colors = ["red", "green", "blue"]
+print(colors[-2])  
+
+numbers = [0, 1, 2, 3, 4, 5]
+subset = numbers[1:4]  
+print(subset) 
