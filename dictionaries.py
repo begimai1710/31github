acf4d1ec8d9ea5d5b2773ef70562e6931bf86598
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-02 22:10:11 +0600

    dictionaries.py new

---
+++ dictionaries.py
@@ -0,0 +1,29 @@
+apartmentapartment= {'alice' : 72, 'bob' : 69, 'charlie' : 80, 'david' : 75}
+print(apartment)
+print(apartment.keys())
+print(apartment.values())
+
+print(apartment['bob'])
+apartment['chris'] = 90
+print (apartment.values())
+del apartment['bob']
+print(apartment)
+apartment['alice'] = 16
+print(apartment.values())
+
+print ('bob' in apartment)
+print ('bob' not in apartment)= {'alice' : 72, 'bob' : 69, 'charlie' : 80, 'david' : 75}
+print(apartment)
+print(apartment.keys())
+print(apartment.values())
+
+print(apartment['bob'])
+apartment['chris'] = 90
+print (apartment.values())
+del apartment['bob']
+print(apartment)
+apartment['alice'] = 16
+print(apartment.values())
+
+print ('bob' in apartment)
+print ('bob' not in apartment)
