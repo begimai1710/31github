ede5a5ce5afbd4dd6476fda45e63933cfa2e0e76
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-04 10:32:42 +0600

    worked on loops

---
+++ loops.py
@@ -0,0 +1,11 @@
+for variable in ["apple", 2, "c"]:
+  print(variable, " is happy")
+
+# is the same as
+
+variable = "apple"
+print(variable, " is happy")
+variable = 2
+print(variable, " is happy")
+variable = "c"
+print(variable, " is happy")
