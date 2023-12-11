776ac4556673dffc8a650940668682f3fdc28d3b
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-11 21:55:35 +0600

    worked on classes

---
+++ classes.py
@@ -0,0 +1,27 @@
+class MyClass:
+  variable = "begimai"
+
+  def function(self):
+    print("This is a message inside the class.")
+
+
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+myobjecty.variable = "is from bishkek"
+print(myobjectx.variable)
+print(myobjecty.variable)
+
+class MyClass:
+  variable = "Skyscanner"
+
+  def function(self):
+    print("This is a message inside the class.")
+
+
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+myobjecty.variable = "is an OTA"
+print(myobjectx.variable)
+print(myobjecty.variable)
