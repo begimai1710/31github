6383ccd5177148b2b85696df9e6d051381a2bb00
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-19 23:08:13 +0600

    added requests.sql
    worked on sql requests (https://www.programiz.com/sql/online-compiler/)

---
+++ requests.sql
@@ -0,0 +1,1 @@
+-- Online SQL Editor to Run SQL Online.-- Use the editor to create new tables, insert data and all other SQL operations.SELECT COUNT(amount) FROM Orders;SELECT AVG(amount) FROM Orders;SELECT MAX(amount) FROM Orders;SELECT MIN(amount) FROM Orders;  SELECT first_name, countryFROM Customers;SELECT last_name, ageFROM Customers WHERE country = 'USA';
