8ab7b98f3dd739040f0c1ac57486c335d86bd27a
begimai1710 <151950926+begimai1710@users.noreply.github.com>
2023-12-21 22:41:19 +0600

    Update requests.sql
    worked more on SQL requests

--- requests.sql
+++ requests.sql
@@ -8,3 +8,13 @@ FROM Customers;
 
 SELECT last_name, age
 FROM Customers WHERE country = 'USA';
+
+
+Insert the missing statement to get all the columns from the Customers table.
+SELECT from Customers;
+
+Write a statement that will select the City column from the Customers table.
+SELECT City FROM Customers;
+
+Select all the different values from the Country column in the Customers table.
+SELECT DISTINCT Country FROM Customers;
